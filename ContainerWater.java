9377d79f6d958b6958e06e7cf645aa84b22d5f64
Sathyan E <61688116+Sathyan-Elangovan@users.noreply.github.com>
2020-07-21 20:03:40 +0530

    Problem Description is added

--- ContainerWater.java
+++ ContainerWater.java
@@ -1,3 +1,26 @@
+/** 
+Problem No(On leetcode):11
+Problem :
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+Note: You may not slant the container and n is at least 2.
+
+ 
+
+for image represenation for better understanding, refer leetcode site.
+ 
+
+Example:
+
+Input: [1,8,6,2,5,4,8,3,7]
+Output: 49
+
+
+
+**/
+
+
+
 public class ContainerWater {
     public static void main(String[] args) {
         int[] a={1,8,6,2,5,4,8,3,7};
