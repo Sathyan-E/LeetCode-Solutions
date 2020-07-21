d19a2993aa2f687b4e5c23973cfb2b7c34a8d09c
Sathyan E <61688116+Sathyan-Elangovan@users.noreply.github.com>
2020-07-21 20:00:08 +0530

    Problem Description is added

--- AddBinary.java
+++ AddBinary.java
@@ -1,12 +1,39 @@
+/**
+Problem No(On leetcode):67
+Problem Statement:
+
+Given two binary strings, return their sum (also a binary string).
+
+The input strings are both non-empty and contains only characters 1 or 0.
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+
+ 
+
+Constraints:
+
+    Each string consists only of '0' or '1' characters.
+    1 <= a.length, b.length <= 10^4
+    Each string is either "0" or doesn't contain any leading zero.
+
+
+
+**/
+
 public class AddBinary {
     public static void main(String[] args) {
         String c="101111";
         String d="10";
        System.out.println(addBinary(c,d));
-        
 
-
-        
     }
     public static String addBinary(String a, String b) {
         if(a.equals("0")){
