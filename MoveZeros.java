9262a0a901915f39be77c4808a179be14ffb0cd8
Sathyan E <61688116+Sathyan-Elangovan@users.noreply.github.com>
2020-08-06 13:59:50 +0530

    Interview Questions

---
+++ MoveZeros.java
@@ -0,0 +1,42 @@
+/**
+ * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Note:
+
+    You must do this in-place without making a copy of the array.
+    Minimize the total number of operations.
+    
+ */
+
+
+public class MoveZeros {
+    public static void main(String[] args) {
+        int[] a={0,1,0,3,12};
+        moveZeroes(a);
+    }
+    
+    public static void moveZeroes(int[] nums) {
+        if(nums==null || nums.length==0){
+            return;
+        }
+        int index=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[index]=nums[i];
+                index++;
+            }
+        }
+        while(index<nums.length){
+            nums[index]=0;
+            index++;
+        }
+        
+        
+    }
+    
+}
